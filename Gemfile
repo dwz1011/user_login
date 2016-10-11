a05f35bffcb7002829f09acf9524a31868ff2a94
duwanzhu <1015479445@qq.com>
2016-10-11 15:19:49 +0800

    add i18n

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem 'semantic-ui-sass', '~> 2.2', '>= 2.2.4.0'
+gem 'rails-i18n', '~> 5.0', '>= 5.0.1'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module UserLogin
   class Application < Rails::Application
+  	config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+  	config.i18n.available_locales = ['zh-CN', :en]
+  	config.i18n.default_locale = 'zh-CN'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
