6f9b0205f2439cca91830c5eb8dbf59640699113
duwanzhu <1015479445@qq.com>
2016-10-12 15:03:39 +0800

    Users资源

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'user/new'
+  get 'users/new'
+
+  resources :users
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
