4041c8df573e8dff2de1eba688bfd8b08aa04e3e
duwanzhu <1015479445@qq.com>
2016-10-12 14:17:59 +0800

    添加密码字段,加密

--- Gemfile
+++ Gemfile
@@ -25,7 +25,7 @@ gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 gem 'semantic-ui-sass', '~> 2.2', '>= 2.2.4.0'
 gem 'rails-i18n', '~> 5.0', '>= 5.0.1'
 gem 'paranoia', '~> 2.2.0.pre'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,21 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
 #
 
 class User < ApplicationRecord
-	vilidates :name, presence: true, length: { maximum: 50 }
-	vilidates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d]+\.[a-z]+\z/i
+	validates :name, presence: true, length: { maximum: 50 }
+	validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness:  { case_sensitive: false }
 
 end
