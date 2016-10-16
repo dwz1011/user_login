911a0b588c33c46abb97f6511a9e39b12efd14a7
duwanzhu <1015479445@qq.com>
2016-10-16 14:07:25 +0800

    email验证

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@
 #
 
 class User < ApplicationRecord
-	before_save { self.email = email.downcase }
+	before_save { self.email = email.downcase }	#把用户的email地址转换成小写
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness:  { case_sensitive: false }
