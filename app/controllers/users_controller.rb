b3f4beaf6b94ff9279176aad629190c5c93ea3b2
duwanzhu <1015479445@qq.com>
2016-10-17 16:15:32 +0800

    注册失败

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,19 @@ class UsersController < ApplicationController
   	# debugger 访问/users/1时，服务器的输出中会显示byebug,可当作控制台，在其中执行代码，ctrl+d退出
   end
 
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		pass
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
