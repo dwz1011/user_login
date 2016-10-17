ac6d508dada09f634efa168d3dc08509522accf0
duwanzhu <1015479445@qq.com>
2016-10-17 23:15:58 +0800

    登录

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,10 @@
 module SessionsHelper
+	#登录指定的用户
+	def log_in(user)
+		session[:user_id] = user.id
+	end
+	#返回当前登录的用户（如果有的话）
+	def current_user
+		@current_user ||= User.find_by(id: session[:user_id])
+	end
 end
