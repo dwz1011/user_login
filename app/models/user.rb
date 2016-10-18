e5edd9708be434534cec69f8be4e2c11a735bc8f
duwanzhu <1015479445@qq.com>
2016-10-18 11:35:48 +0800

    记住用户

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,14 @@
 #
 # Table name: users
 #
-#  id              :integer          not null, primary key
-#  name            :string(255)
-#  email           :string(255)
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
-#  password_digest :string(255)
+#  id               :integer          not null, primary key
+#  name             :string(255)
+#  email            :string(255)
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  password_digest  :string(255)
+#  remember_ddigest :string(255)
+#  remember_digest  :string(255)
 #
 # Indexes
 #
@@ -15,6 +17,7 @@
 #
 
 class User < ApplicationRecord
+	attr_accessor :remember_token
 	before_save { self.email = email.downcase }	#把用户的email地址转换成小写
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d]+\.[a-z]+\z/i
@@ -27,5 +30,16 @@ class User < ApplicationRecord
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
 													  BCrypt::Engine.cost
 		BCrypt::password.create(string, cost: cost)
+	end
+
+	#返回一个随机令牌
+	def User.new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	#在数据库中记住用户
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
 	end
 end
