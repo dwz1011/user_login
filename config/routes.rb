33fc6784059688faa61c6641e3059f37c3f843cc
duwanzhu <1015479445@qq.com>
2016-10-18 09:58:25 +0800

    页面部署

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  get  '/help',    to: 'static_pages#help', as: 'helf'
+  get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
 
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   # get 'session/new'
 
-  resources :users
+  # resources :users
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   get  '/login',   to: 'sessions#new'
