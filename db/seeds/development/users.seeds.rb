2665645f2afd440821077718d4747042d741076b
dwz1011 <1015479445@qq.com>
2016-10-21 10:48:28 +0800

    2016

--- db/seeds/development/users.seeds.rb
+++ db/seeds/development/users.seeds.rb
@@ -1,10 +1,3 @@
-# User.create!(name: "Michael Hartl", email: "mhartl@example.com", password_digest: 111111, password_confirmation: 111111)
-# User.create(name: "Jon Hartl", email: "jhartl@example.com", password_digest: 111111, password_confirmation: 111111)
-# User.create(name: "Jon1 Hartl", email: "j1hartl@example.com", password_digest: 111111, password_confirmation: 111111)
-# User.create(name: "Jon2 Hartl", email: "j2hartl@example.com", password_digest: 111111, password_confirmation: 111111)
-# User.create(name: "Jon3 Hartl", email: "j3hartl@example.com", password_digest: 111111, password_confirmation: 111111)
-# User.create(name: "Jon4 Hartl", email: "j4hartl@example.com", password_digest: 111111, password_confirmation: 111111)
-# User.create(name: "Jon5 Hartl", email: "j5hartl@example.com", password_digest: 111111, password_confirmation: 111111)
 
 User.create!(name:  "Example User",
              email: "example0@railstutorial.org",
